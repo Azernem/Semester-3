45719f7a287e5069f7a1e901b6e49c5bcabf20d5
Azernem <Azernem@gmail.com>
2024-09-16 02:17:39 +0300

    added new program about Parallel Multiplication

---
+++ ParallelMultiply/ParallelMultiply.Tests/ParallelMultiply.Tests.cs
@@ -0,0 +1,142 @@
+// <copyright file="ParallelMultiply.cs" company="NematMusaev">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+using ParallelMultiplication;
+
+namespace ParallelMultiply.Tests;
+
+/// <summary>
+/// tests matrixes and operation of matrixes.
+/// </summary>
+public class Tests
+{
+    /// <summary>
+    /// test about parallel multiplication.
+    /// </summary>
+    [Test]
+    public void ParallelMultiplyTest()
+    {
+        var checkMatrix = new int[,] {{5, 12}, {1, 8}};
+        string path1 = "../../../matrix1.txt";
+        string path2 = "../../../matrix2.txt";
+        var fileMatrix1 = new FileMatrix(path1);
+        fileMatrix1.CreateMatrix();
+        var fileMatrix2 = new FileMatrix(path2);
+        fileMatrix2.CreateMatrix();
+        var matrixOperation = new MatrixOperation(fileMatrix1.matrix, fileMatrix2.matrix);
+        Assert.That(matrixOperation.ParallelMultiply(), Is.EqualTo(checkMatrix));
+    }
+    
+    /// <summary>
+    /// Test about parallel multiplication.
+    /// </summary>
+    [Test]
+    public void SequentallyMultiplyTest()
+    {
+        var checkMatrix = new int[,] {{5, 12}, {1, 8}};
+        string path1 = "../../../matrix1.txt";
+        string path2 = "../../../matrix2.txt";
+        var fileMatrix1 = new FileMatrix(path1);
+        fileMatrix1.CreateMatrix();
+        var fileMatrix2 = new FileMatrix(path2);
+        fileMatrix2.CreateMatrix();
+        var matrixOperation = new MatrixOperation(fileMatrix1.matrix, fileMatrix2.matrix);
+        Assert.That(matrixOperation.SequentiallyMultiply(), Is.EqualTo(checkMatrix));
+    }
+    
+    /// <summary>
+    /// Test about correct size matrix.
+    /// </summary>
+    [Test]
+    public void IncorrectSizeMatrixTest()
+    {
+        string path1 = "../../../IncorrectSize.txt";
+        var fileMatrix1 = new FileMatrix(path1);
+        Assert.Throws<IncorrrectSizeException>(() => fileMatrix1.CreateMatrix());
+    }
+    
+    /// <summary>
+    /// Test about right size matrix.
+    /// </summary>
+    [Test]
+    public void SizeMatrixTest()
+    {
+        string path1 = "../../../Size.txt";
+        var fileMatrix = new FileMatrix(path1);
+        fileMatrix.CreateMatrix();
+        Assert.That(fileMatrix.matrix.GetLongLength(0), Is.EqualTo(2));
+        Assert.That(fileMatrix.matrix.GetLongLength(1), Is.EqualTo(3));
+    }
+    
+    /// <summary>
+    /// Test about relevant element type of matrix.
+    /// </summary>
+    [Test]
+    public void CheckMatrixTypeTest()
+    {
+        string path1 = "../../../CheckMatrixType.txt";
+        var fileMatrix = new FileMatrix(path1);
+        Assert.Throws<AnotherTypeException>(() => fileMatrix.CreateMatrix());
+    }
+    
+    /// <summary>
+    /// Test about compatibility of two matrixes.
+    /// </summary>
+    [Test]
+    public void CompatibilityExceptionTest()
+    {
+        string path1 = "../../../matrix1.txt";
+        string path2 = "../../../Size.txt";
+        var fileMatrix1 = new FileMatrix(path1);
+        fileMatrix1.CreateMatrix();
+        var fileMatrix2 = new FileMatrix(path2);
+        fileMatrix2.CreateMatrix();
+        var matrixOperation = new MatrixOperation(fileMatrix1.matrix, fileMatrix2.matrix);
+        Assert.Throws<СompatibilityException>(() => matrixOperation.ParallelMultiply());
+    }
+    
+    /// <summary>
+    /// Test wich generates matrix.
+    /// </summary>
+    [Test]
+    public void GenerateMatrixTest()
+    {
+        var (sizeRows, sizeColumns) = (3, 3);
+        var matrix = FileMatrix.GenerateMatrix(sizeRows, sizeColumns);
+        Assert.That(matrix.GetLength(0), Is.EqualTo(sizeRows));
+    }
+    
+    /// <summary>
+    /// Checkes compatibility of matrixes.
+    /// </summary>
+    [Test]
+    public void EqualsTest()
+    {
+        var matrix1 = new int[,] {{1, 2 , 3}, {3, 3, 3}};
+        var matrix2 = new int[,] {{1, 2}, {3, 3}};
+        var matrixOperation = new MatrixOperation(matrix1, matrix2);
+        Assert.Throws<СompatibilityException>(() => matrixOperation.Combine());
+    }
+    
+    /// <summary>
+    /// Gets file with particular matrix.
+    /// </summary>
+    [Test]
+    public void GetFileWithResult()
+    {
+        var resultPath = "../../../resultmatrix.txt"; 
+        var checkMatrix = new int[,] {{5, 12}, {1, 8}};
+        string path1 = "../../../matrix1.txt";
+        string path2 = "../../../matrix2.txt";
+        var fileMatrix1 = new FileMatrix(path1);
+        fileMatrix1.CreateMatrix();
+        var fileMatrix2 = new FileMatrix(path2);
+        fileMatrix2.CreateMatrix();
+        var matrixOperation = new MatrixOperation(fileMatrix1.matrix, fileMatrix2.matrix);
+        FileMatrix.WriteToFile(matrixOperation.ParallelMultiply(), resultPath);
+        var fileResultmatrix = new FileMatrix(resultPath);
+        fileResultmatrix.CreateMatrix();
+        var resultmatrix = fileResultmatrix.matrix;
+        Assert.That(resultmatrix, Is.EqualTo(checkMatrix));
+    }
+}
---
+++ ParallelMultiply/ParallelMultiply/Exceptions.cs
@@ -0,0 +1,44 @@
+// <copyright file="ParallelMultiply.cs" company="NematMusaev">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace ParallelMultiplication;
+
+public class IncorrrectSizeException: Exception
+{
+    public IncorrrectSizeException(string message): base(message)
+    {
+
+    }
+}
+
+public class AnotherTypeException: Exception
+{
+    public AnotherTypeException(string message): base(message)
+    {
+
+    }
+}
+
+public class СompatibilityException: Exception
+{
+    public СompatibilityException(string message): base(message)
+    {
+
+    }
+}
+
+public class ExistenceFileLoadException: Exception
+{
+    public ExistenceFileLoadException(string message): base(message)
+    {
+
+    }
+}
+
+public class EmptyFileException: Exception
+{
+    public EmptyFileException(string message): base(message)
+    {
+
+    }
+}
