4d299917446b8aff34daef63432870488618e851
Azernem <Azernem@gmail.com>
2024-09-16 02:22:59 +0300

    added right coprightes

--- ParallelMultiply/ParallelMultiply/Exceptions.cs
+++ ParallelMultiply/ParallelMultiply/Exceptions.cs
@@ -1,4 +1,4 @@
-// <copyright file="ParallelMultiply.cs" company="NematMusaev">
+// <copyright file="Exceptions.cs" company="NematMusaev">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 namespace ParallelMultiplication;
