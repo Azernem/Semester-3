78e2985277c1e01ff429e0e8cba5deebea4dd23e
Azernem <Azernem@gmail.com>
2024-10-11 02:04:03 +0300

    added program about pool

---
+++ MyThreadPool/MyThreadPool/IMyTaskResult.cs
@@ -0,0 +1,19 @@
+// <copyright file="IMyTaskResult.cs" company="NematMusaev">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+using System.Collections;
+
+namespace MyThreadPool;
+
+/// <summary>
+/// Interface of task with Result.
+/// </summary>
+/// <typeparam name="Tres">Result of task. </typeparam>
+public interface IMyTask<Tres>
+{
+    public bool IsCompleted {get; set; }
+
+    public Tres? Result {get; }
+
+    public IMyTask<TNewRes> ContinueWith<TNewRes>(Func<Tres, TNewRes> func);
+}
